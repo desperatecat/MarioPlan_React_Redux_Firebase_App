994b60a2195e99201204edc54633495bc60fe9f5
desperatecat <376198935@qq.com>
2019-12-17 18:30:55 -0500

    Navbar setup

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render(){
@@ -8,7 +9,7 @@ class App extends Component {
       <BrowserRouter>
       
         <div className="App">
-          <h1>MarioPlan</h1>
+          <Navbar />
         </div>
 
       </BrowserRouter>
