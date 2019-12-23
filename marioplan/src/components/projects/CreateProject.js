baa20c1d651c2fb361c7f7c90e25a255fe9e1fc1
desperatecat <376198935@qq.com>
2019-12-22 21:46:17 -0500

    dispatch action creators

--- marioplan/src/components/projects/CreateProject.js
+++ marioplan/src/components/projects/CreateProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
+
 
 class CreateProject extends Component {
     state = {
@@ -38,4 +41,10 @@ class CreateProject extends Component {
     }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect()(CreateProject)
