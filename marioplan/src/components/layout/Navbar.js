424718028c6297e4dcea290bfc08c26a864c5fe9
desperatecat <376198935@qq.com>
2019-12-31 13:00:00 -0500

    User Profile Data

--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -5,9 +5,9 @@ import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
 const Navbar = (props) => {
-  const { auth } = props;
+  const { auth, profile } = props;
 
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
 
 
   return (
@@ -22,8 +22,11 @@ const Navbar = (props) => {
 
 const mapStateToProps = (state) => {
 
+  console.log(state);
+
   return{
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   }
 }
 
