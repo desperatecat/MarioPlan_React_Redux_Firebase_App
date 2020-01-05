ac1eb4f6bebb2f47a6d815b01864f971aec926f4
desperatecat <376198935@qq.com>
2020-01-05 12:32:54 -0500

    deploy to firebase host

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React-Redux-Firebase-App
-React-Redux-Firebase-App: a simple project management tool with authentication and cloud functions.
+React-Redux-Firebase-App: a simple project management tool with authentication and cloud functions. User can sign up, login, log out from the application, and create customer project after logging in.
+
+The application is already deployed, visit https://lucas-marioplan.firebaseapp.com/ to check it out!
---
+++ marioplan/dist/precache-manifest.da9a7494f4467dd167c7c8d2a13e787f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e154c9a285585adaa2b3eacb697b722f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61052523eff3c80deba1",
+    "url": "/static/css/main.53aa6dca.chunk.css"
+  },
+  {
+    "revision": "e1410850d5a28361196e",
+    "url": "/static/js/2.46dc66f1.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/static/js/2.46dc66f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61052523eff3c80deba1",
+    "url": "/static/js/main.ea6f0189.chunk.js"
+  },
+  {
+    "revision": "a5b2ff1f6e078830f72b",
+    "url": "/static/js/runtime-main.503e589e.js"
+  }
+]);
